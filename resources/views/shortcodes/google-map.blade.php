da3f3edeba9885a9a5fabc2b3d17df3fefa3505b
Tec-Dynamics <support@tecdynamics.co.uk>
2023-10-18 11:44:46 +0100

    Update google-map.blade.php

--- resources/views/shortcodes/google-map.blade.php
+++ resources/views/shortcodes/google-map.blade.php
@@ -1,6 +1,9 @@
-<div style="height: 400px; width: 100%; position: relative; text-align: right;">
-    <div  style="height: 400px; width: 100%; overflow: hidden; background: none!important;">
-        <iframe width="100%" height="500" src="https://maps.google.com/maps?q={{ addslashes($address) }}%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
+<div class="@if($class) {{$class}} @else {{'google-map'}} @endif d-flex"
+     style=" @if($style) {{$style}}; @else {{'height:400px;width: 100%; position: relative; text-align: right;'}} @endif ">
+    <div class="google-map-wrapper w-100"
+         style="@if($style) {{$style}} @else {{'height:400px; width: 100%; overflow: hidden; background: none!important;'}} @endif   ">
+        <iframe style='height: 100%; width: 100%;'
+                src="https://maps.google.com/maps?q={{ addslashes($address) }}%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
                 frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
     </div>
 </div>
