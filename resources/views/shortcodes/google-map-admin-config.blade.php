0d3012c9f89605406dd2f07cd46da2a96b47f8b1
Tec-Dynamics <support@tecdynamics.co.uk>
2023-10-18 11:44:34 +0100

    Update google-map-admin-config.blade.php

--- resources/views/shortcodes/google-map-admin-config.blade.php
+++ resources/views/shortcodes/google-map-admin-config.blade.php
@@ -2,3 +2,15 @@
     <label class="control-label">{{ __('Address') }}</label>
     {!! Form::input('text', 'address', $content, ['class' => 'form-control', 'data-shortcode-attribute' => 'content', 'placeholder' => '24 Roberts Street, SA73, Chester']) !!}
 </div>
+    <div class="form-group mb-3">
+        <label class="control-label">{{ __('Class') }}</label>
+        {!! Form::input('text', 'class', $class, ['class' => 'form-control', 'data-shortcode-attribute' => 'class', 'placeholder' => 'col-5 w-100']) !!}
+    </div>
+        <div class="form-group mb-3">
+            <label class="control-label">{{ __('Style') }}</label>
+            {!! Form::input('text', 'style', $style, ['class' => 'form-control', 'data-shortcode-attribute' => 'style', 'placeholder' => 'height:400px;width: 100%; position: relative; text-align: right;']) !!}
+</div>
+<div class="form-group mb-3">
+            <label class="control-label">{{ __('Map Height') }}</label>
+            {!! Form::input('text', 'height', $height, ['class' => 'form-control', 'data-shortcode-attribute' => 'height', 'placeholder' => '400 all is pixels']) !!}
+</div>
